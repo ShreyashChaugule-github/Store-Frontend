dd389ae35b9ac60cd3f5c2a1887df6fa095be0ef
APPLE <cshreyash219@gmail.com>
2025-05-29 13:37:20 +0530

    rerendering error

--- app/(routes)/cart/page.tsx
+++ app/(routes)/cart/page.tsx
@@ -1,4 +1,6 @@
 "use client"
+export const dynamic = "force-dynamic"; 
+
 import { useEffect, useState } from "react";
 import Container from "@/components/ui/container";
 import useCart from "@/hooks/use-cart";
